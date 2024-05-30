f7da711828eee50b0046b435d08f067e664ec3a9
mingdy52 <mincount552@gmail.com>
2024-05-30 13:22:49 +0900

    prime number

--- argorithm/study/week1/기초수학/1929_prime_number.java
+++ argorithm/study/week1/기초수학/1929_prime_number.java
@@ -11,23 +11,35 @@ class Main {
 		int m = Integer.parseInt(nums[0]);
 		int n = Integer.parseInt(nums[1]);
 
-		boolean[] isPrime = new boolean[n + 1];
+        // 에라토스테네스의 체
+        // 1. 2부터 n까지의 모든 숫자를 나열
+        // 2. 2: 소수. 리스트에 추가하고, 2의 배수를 삭제
+        // 3. 3: 다음 소수. 3을 리스트에 추가하고, 3의 배수를 삭제
+        // 반복하면 소수만 남게 됨
 
-		for(int i = 2; i <= n; i++) {
-			isPrime[i] = true;
-		}
-
-		for(int p = 2; p * p <= n; p++) {
-			if(isPrime[p]) {
-				for(int j = p *p; j <= n; j += p) {
-					isPrime[j] = false;
-				}
-			}
-		}
-
+		boolean[] isPrime = isPrime(n);
+        primeProcess(isPrime, n);
 		for(int i = m; i <= n; i++) {
 			if(isPrime[i]) {
 				System.out.println(i);
+			}
+		}
+    }
+
+    public static boolean[] isPrime(int n) {
+        boolean[] isPrime = new boolean[n + 1]; // 인덱스를 편하게 하기 위해 n + 1로 설정
+		for(int i = 2; i <= n; i++) { // 0, 1 은 소수가 아니니까 2부터 시작
+			isPrime[i] = true; // 일단 모두 소수라고 가정
+		}
+        return isPrime;
+    }
+    
+    public static void primeProcess(boolean[] isPrime, int n) {
+		for(int p = 2; p * p <= n; p++) { // p의 배수가 n보다 작거나 같을 때까지
+			if(isPrime[p]) {
+				for(int j = p * p; j <= n; j += p) { //p의 배수를 제거
+					isPrime[j] = false;
+				}
 			}
 		}
     }
