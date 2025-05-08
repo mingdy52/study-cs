8ee738aacd234584c491ddcf8f1b6eefb0e03eaa
mingdy <mincount552@gmail.com>
2025-05-08 22:53:24 +0900

    ConvertSnakeToPascalCase

---
+++ cs/src/main/java/etc/ConvertSnakeToPascalCase.java
@@ -0,0 +1,23 @@
+package etc;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class ConvertSnakeToPascalCase {
+
+    public static void main(String[] args) throws IOException {
+        convertSnakeToPascalCase("find_prime_list_under_number");
+    }
+
+
+    public static void convertSnakeToPascalCase(String snakeCase) {
+        String pascalCase = Arrays.stream(snakeCase.split("_"))
+                .filter(s -> !s.isEmpty())
+                .map(s -> Character.toUpperCase(s.charAt(0)) + s.substring(1))
+                .collect(Collectors.joining());
+
+        System.out.println(pascalCase);
+    }
+
+}
